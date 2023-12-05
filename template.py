4ee55bdabcf0ea220fe793abb8a2ec039dd37961
Tylor Childers <tylord1@gmail.com>
2023-12-04 19:17:08 -0500

    Update template.py

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import requests
 
 DAY = 2
 
-inp = requests.get(f"https://adventofcode.com/2023/day/{DAY}/input", headers={'cookie':'session=53616c7465645f5fdd43845c5356a0c1945ec6af39857de05986f0b1429dcec01d14f21459ebed0f9abfbfcff006c9e3b3282785b66d2d9fbb4969cb6b69e296'}).text.split('\n')
+inp = requests.get(f"https://adventofcode.com/2023/day/{DAY}/input", headers={'cookie':'session='}).text.split('\n')
 
 for l in inp:
   print(l)
